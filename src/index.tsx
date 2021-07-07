1f3ac01ef30dd3e8603e367ac81e1ee1db4ceb1d
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-07-07 12:01:51 -0300

    configuring miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Router from "./routes";
 
+import { createServer } from "miragejs";
+
 import GlobalStyles from "./styles/GlobalStyles";
+
+createServer({
+  routes() {
+    //rotas
+    this.namespace = "api"; //ao captar os dados da api pelo front, irá trazer dados a partir do namespace "api"
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transacion 1",
+          amount: 400,
+          type: "deposit",
+          category: "food",
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
